ce555072ec42416da1b37fe47cc4b1d12b425fbf
ben yeger <benyager36@gmail.com>
2019-12-25 17:43:27 +0200

    17:43 ben

--- cmps/Sidebar.jsx
+++ cmps/Sidebar.jsx
@@ -8,9 +8,9 @@ export default class SideBar extends React.Component {
             <ul >
                 <li><Link   to='/email/Inbox' >Inbox </Link></li>
                 <li>Strret</li>
-                <li>Strret</li>
+                <li>Sent mail</li>
 
-                <li>Strret</li>
+                <li>Drafts</li>
 
                 {/* <li><Link  to='/Sent mail'>Sent mail</Link></li>
                 <li><Link   to='/Drafts'>Drafts</Link></li> */}
