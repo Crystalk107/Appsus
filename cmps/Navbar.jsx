04739cec869ed71c2573973010372279d3aa677b
Crystalk107 <30338496+Crystalk107@users.noreply.github.com>
2019-12-25 12:19:36 +0200

    nav main.css home.jsx

--- cmps/Navbar.jsx
+++ cmps/Navbar.jsx
@@ -1,11 +1,11 @@
 const { NavLink } = ReactRouterDOM
 export default function NavBar(props) {
     return <nav>
-        <ul>
-            <li><NavLink activeClassName="active" to='/' exact>Home</NavLink></li>
-            <li><NavLink activeClassName="active" to='/mrEmail'>misterEmail</NavLink></li>
-            <li><NavLink activeClassName="active" to='/msBooks'>missBooks</NavLink></li>
-            <li><NavLink activeClassName="active" to='/msKeep'>missKeep</NavLink></li>
+        <ul className="clean-list navbar">
+            <li><NavLink className="link-style" activeClassName="active" to='/' exact>Home</NavLink></li>
+            <li><NavLink  className="link-style" activeClassName="active" to='/mrEmail'>misterEmail</NavLink></li>
+            <li><NavLink className="link-style" activeClassName="active" to='/msBooks'>missBooks</NavLink></li>
+            <li><NavLink className="link-style" activeClassName="active" to='/msKeep'>missKeep</NavLink></li>
         </ul>
     </nav>
 }
---
+++ pages/Home.jsx
@@ -0,0 +1,10 @@
+export default class Home extends React.Component {
+
+    render() {
+        return (
+            <section className="home-container">
+                <h1>Appsus</h1>
+            </section>
+        )
+    }
+}
