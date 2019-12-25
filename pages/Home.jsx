041f0cdadc970857b5b0edcaae48ebfe7fdb5537
Crystalk107 <30338496+Crystalk107@users.noreply.github.com>
2019-12-25 13:22:25 +0200

    css home

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,7 +3,12 @@ export default class Home extends React.Component {
     render() {
         return (
             <section className="home-container">
-                <h1>Appsus</h1>
+                <h1>Appsus - Homepage</h1>
+                <ul className="homeApps-container clean-list">
+                    <li>misterEmail</li>
+                    <li>missKeep</li>
+                    <li>missBook</li>
+                </ul>
             </section>
         )
     }
