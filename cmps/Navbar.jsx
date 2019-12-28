5cfe544aae2f0518d9723fb20565faf805fab10f
Crystalk107 <30338496+Crystalk107@users.noreply.github.com>
2019-12-28 22:34:58 +0200

    styling navbar

--- cmps/Navbar.jsx
+++ cmps/Navbar.jsx
@@ -2,10 +2,10 @@ const { NavLink } = ReactRouterDOM
 export default function NavBar(props) {
     return <nav>
         <ul className="clean-list navbar">
-            <li><NavLink className="link-style" activeClassName="active" to='/' exact>Home</NavLink></li>
-            <li><NavLink  className="link-style" activeClassName="active" to='/email'>misterEmail</NavLink></li>
-            <li><NavLink className="link-style" activeClassName="active" to='/msBooks'>missBooks</NavLink></li>
-            <li><NavLink className="link-style" activeClassName="active" to='/MissKeepApp'>missKeep</NavLink></li>
+            <li><NavLink className="link-style nav-link" activeClassName="active" to='/' exact><i className="fas fa-home"></i> Home</NavLink></li>
+            <li><NavLink  className="link-style nav-link" activeClassName="active" to='/email'><i className="fas fa-at"></i> Email</NavLink></li>
+            <li><NavLink className="link-style nav-link" activeClassName="active" to='/MissKeepApp'><i className="fas fa-paperclip"></i> Keep</NavLink></li>
+            <li><NavLink className="link-style nav-link" activeClassName="active" to='/msBooks'><i className="fas fa-book"></i> Books</NavLink></li>
 
 
             
